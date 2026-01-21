cc653dd3d9357879bf50236191833f1e09979a93
Brenda Tan <brendatan@Brendas-MacBook-Air-2.local>
2026-01-22 01:12:47 +0800

    Add mark and unmark task feature

--- src/main/java/Candy.java
+++ src/main/java/Candy.java
@@ -4,7 +4,7 @@ public class Candy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int noTask = 0;
 
         System.out.println("Hello! I'm Candy");
@@ -12,16 +12,33 @@ public class Candy {
 
         while (true) {
             String input = scanner.nextLine();
-
+            // EXIT
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals("list")) {
-                for (int i = 0; i < noTask ; i++) {
+            } else if (input.equals("list")) { //LIST
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < noTask; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
-            } else {
-                tasks[noTask] = input;
+            } else if (input.startsWith("mark")) { // MARK DONE
+                String[] parts = input.split(" ");
+                int idx = Integer.parseInt(parts[1]) - 1;
+
+                tasks[idx].markDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[idx]);
+            } else if (input.startsWith("unmark")) { // UNMARK
+                String[] parts = input.split(" ");
+                int idx = Integer.parseInt(parts[1]) - 1;
+
+                tasks[idx].unmark();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[idx]);
+            } else { //ADD TASK
+                tasks[noTask] = new Task(input);
                 noTask++;
 
                 System.out.println("added: " + input);
