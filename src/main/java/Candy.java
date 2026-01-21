45bf840816f379f08580f9592335fd58a2591d7b
Brenda Tan <brendatan@Brendas-MacBook-Air-2.local>
2026-01-22 00:16:57 +0800

    Level-1 echo

--- src/main/java/Candy.java
+++ src/main/java/Candy.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Candy {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Candy");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        scanner.close();
     }
 }
